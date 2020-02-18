6e08ad561700f2aae9da9b08c7da1d7964dea333
Ognjen Civcic <civcic.ra167.2017@uns.ac.rs>
2020-02-18 20:52:39 +0100

    #trie
    napravljena klasa trie i funkcije za dodavanje i pretragu

---
+++ Python_projekat/Strukture/trie.py
@@ -0,0 +1,63 @@
+
+
+class TrieNode:
+    def __init__(self, slovo = None):
+        self.slovo = slovo
+        self.fajlovi = {}
+        self.deca = {}
+
+    def __getitem__(self, slovo):
+        return self.deca[slovo]
+
+    def dodajCvorDete(self,slovo):
+        self.deca[slovo] = TrieNode(slovo)
+
+class Trie:
+    def __init__(self):
+        self.koren = TrieNode()
+
+    def __getitem__(self, data):
+        return self.koren.deca[data]
+
+    def dodajRec(self,rec,fajl):
+        """
+        Funkcija dodaje reci iz fajlova koji se parsiraju u trie
+        :param rec: rec koja se dodaje u trie
+        :param fajl: fajl iz kog se dodaje rec
+        :return:
+        """
+        cvor = self.koren
+        recPostoji = True
+
+        for i in range(len(rec)):
+            if rec[i] in cvor.deca:
+                cvor = cvor.deca[rec[i]]
+            else:
+                recPostoji = False
+                break
+
+        if not recPostoji:
+            while i < len(rec):
+                cvor.dodajCvorDete(rec[i])
+                cvor = cvor.deca[rec[i]]
+                i += 1
+
+        if fajl not in cvor.fajlovi:
+            cvor.fajlovi[fajl] = 1 # Ako se fajl ne nalazi u recniku fajlova postavlja broj reci u njemu na 1, u suprotnom uvecava za 1
+        else:
+            cvor.fajlovi[fajl] += 1
+
+    def pronadjiRec(self, rec):
+        """
+
+        :param rec: rec koja se trazi
+        :return: vraca fajlove u kojima se nalazi trazena rec
+        """
+        cvor = self.koren
+        for char in rec:
+            if char in cvor.deca:
+                cvor = cvor.deca[char]
+            else:
+                return None
+
+        return cvor.fajlovi
