82639de63c7e488f0b15e64512c405aab4a18e51
Bojan Vujic <vujic.ra170.2017@uns.ac.rs>
2020-02-22 21:24:54 +0100

    #rangirana_pretraga

--- Python_projekat/funkcionalnosti.py
+++ Python_projekat/funkcionalnosti.py
@@ -25,6 +25,49 @@ def load(directory):
                     graph.insert_edge(cvor1, cvor2)
 
 
+def ranking(documents, dict1, dict2=None):
+    result = []
+    pom = []
+    for document in documents:
+
+        vertex = graph.get_vertex(document)
+        links_number = graph.degree(vertex, False)
+        links = links_number * 0.8
+        if document not in dict1.keys():
+            words_doc1 = 0
+        else:
+            words_doc1 = dict1[document]
+        if  dict2 is None or document not in dict2.keys():
+            words_doc2 = 0
+        else:
+            words_doc2 = dict2[document]
+        words = words_doc1 * 0.2 + words_doc2 * 0.2
+        words_in_links = 0
+        edges = graph.incident_edges(vertex, False)
+        for edge in edges:
+            doc = edge.origin.get_element
+            if doc not in dict1.keys():
+                word1 = 0
+            else:
+                word1 = dict1[doc]
+            if doc not in dict1.keys():
+                word2 = 0
+            else:
+                word2 = dict2[doc]
+            words_in_links += word1 + word2
+        words_in_links = words_in_links * 1.4
+        pom.append(str(document))
+        pom.append(words + links + words_in_links)
+        print(pom)
+        result.append(pom)
+        pom.pop(1)
+        pom.pop(0)
+    return result
+
+
+
+
+
 directory = str(input("Unesi korenski direktorijum"))
 start_time = time.time()
 load(directory)
